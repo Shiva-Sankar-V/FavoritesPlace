d48f588e37c9129e394cc4b4c3e6cd009e903d5e
Shiva Sankar <shivasankar19@outlook.com>
2023-07-08 12:23:49 +0530

    created a icon for adding new place

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
+import IconButton from "./components/UI/IconButton";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,7 +14,15 @@ export default function App() {
       <StatusBar style="dark" />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="All Places" component={AllPlaces} />
+          <Stack.Screen
+            name="All Places"
+            component={AllPlaces}
+            options={{
+              headerRight: ({ tintColor }) => (
+                <IconButton icon="add" size={24} color={tintColor} />
+              ),
+            }}
+          />
           <Stack.Screen name="Add Place" component={AddPlace} />
         </Stack.Navigator>
       </NavigationContainer>

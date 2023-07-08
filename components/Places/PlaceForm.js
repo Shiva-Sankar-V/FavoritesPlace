746926773951a9381f35cbff4c58f37df4f393f9
Shiva Sankar <shivasankar19@outlook.com>
2023-07-08 11:35:56 +0530

    Add comp to define the details reqd for new place

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,14 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
 
-const AddPlace = () => {
+const PlaceForm = () => {
   return (
     <View>
-      <Text>AddPlace</Text>
+      <Text>PlaceForm</Text>
     </View>
   );
 };
 
-export default AddPlace;
+export default PlaceForm;
 
 const styles = StyleSheet.create({});

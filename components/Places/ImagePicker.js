ef4c5add2eccac0f949fd3556383a444482001e6
Shiva Sankar <shivasankar19@outlook.com>
2023-07-08 13:17:38 +0530

    created helper fun for taking img

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,12 +1,17 @@
 import { Button } from "react-native";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { launchCameraAsync } from "expo-image-picker";
 
 const ImagePicker = () => {
+  async function takeImageHandler() {
+    const image = await launchCameraAsync();
+    console.log(image);
+  }
+
   return (
     <View>
       <View></View>
-      <Button title="Take Image" />
-      <Text>ImagePicker</Text>
+      <Button title="Take Image" onPress={takeImageHandler} />
     </View>
   );
 };

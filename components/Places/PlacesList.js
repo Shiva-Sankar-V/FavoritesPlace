b03440c7f13dc4829c5902c375c8de43f139d15b
Shiva Sankar <shivasankar19@outlook.com>
2023-07-08 12:42:06 +0530

    changed the style for fallbacktext & icon button

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,5 +1,6 @@
 import { FlatList, StyleSheet, View, Text } from "react-native";
 import PlaceItem from "./PlaceItem";
+import { Colors } from "../../constants/colors";
 
 const PlacesList = ({ places }) => {
   if (!places || places.length === 0) {
@@ -29,5 +30,6 @@ const styles = StyleSheet.create({
   fallbackText: {
     fontSize: 16,
     fontWeight: "bold",
+    color: Colors.primary200,
   },
 });

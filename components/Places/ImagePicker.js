ba47f99f724124a3fa634c5e5e2e47a85110c9c0
Shiva Sankar <shivasankar19@outlook.com>
2023-07-08 13:10:43 +0530

    add general disp for image & button for taking img

---
+++ components/Places/ImagePicker.js
@@ -0,0 +1,16 @@
+import { Button } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+
+const ImagePicker = () => {
+  return (
+    <View>
+      <View></View>
+      <Button title="Take Image" />
+      <Text>ImagePicker</Text>
+    </View>
+  );
+};
+
+export default ImagePicker;
+
+const styles = StyleSheet.create({});
